94f75109f007c704011b78d13b02fa49d78b5939
maitenala <maitenala123@gmail.com>
2023-06-15 23:17:47 -0300

    Se crea archivo .json y se reemplaza lista html

--- js/buscador.js
+++ js/buscador.js
@@ -1,28 +1,38 @@
+// Obtener referencias a los elementos del DOM
 const buscador = document.getElementById('buscador');
 const listaPeliculas = document.getElementById('listaPeliculas');
 
-buscador.addEventListener('input', function() {
-    if (buscador.value.length > 0) {
-    listaPeliculas.style.display = 'block';
-} else {
-    listaPeliculas.style.display = 'none';
-}
-});
+// Cargar los datos desde el archivo JSON
+fetch('data/buscador.json')
+.then(response => response.json())
+.then(data => {
+    // Almacenar los datos en una variable
+    const peliculas = data;
 
-document.addEventListener("keyup", e=>{
-
-    if (e.target.matches("#buscador")){
-
-        if (e.key ==="Escape")e.target.value = ""
-
-        document.querySelectorAll(".pelicula").forEach(peliculas =>{
-
-            peliculas.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-                ?peliculas.classList.remove("filtro")
-                :peliculas.classList.add("filtro")
-        })
-
+    // Función para mostrar las películas filtradas en la lista
+    function mostrarPeliculas(peliculasFiltradas) {
+    listaPeliculas.innerHTML = '';
+    peliculasFiltradas.forEach(pelicula => {
+        const li = document.createElement('li');
+        li.textContent = pelicula.titulo;
+        listaPeliculas.appendChild(li);
+    });
+    listaPeliculas.style.display = peliculasFiltradas.length > 0 ? 'block' : 'none';
     }
 
-
+    // Evento input del campo de búsqueda
+    buscador.addEventListener('input', function() {
+        const filtro = buscador.value.toLowerCase();
+        const peliculasFiltradas = peliculas.filter(pelicula =>
+            pelicula.titulo.toLowerCase().includes(filtro)
+        );
+    mostrarPeliculas(peliculasFiltradas);
+    });
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target !== buscador && target !== listaPeliculas) {
+            listaPeliculas.style.display = 'none';
+        }
+    });
 })
+.catch(error => console.error('Error al cargar las películas:', error));
