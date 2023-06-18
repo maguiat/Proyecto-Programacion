00d10f75102011b633173ce4be82d6ecb3d6bede
maitenala <maitenala123@gmail.com>
2023-06-17 22:43:21 -0300

    Agregando estilos a la lista y al scroll

--- js/buscador.js
+++ js/buscador.js
@@ -1,15 +1,11 @@
-// Obtener referencias a los elementos del DOM
 const buscador = document.getElementById('buscador');
 const listaPeliculas = document.getElementById('listaPeliculas');
 
-// Cargar los datos desde el archivo JSON
 fetch('data/buscador.json')
 .then(response => response.json())
 .then(data => {
-    // Almacenar los datos en una variable
     const peliculas = data;
 
-    // Función para mostrar las películas filtradas en la lista
     function mostrarPeliculas(peliculasFiltradas) {
     listaPeliculas.innerHTML = '';
     peliculasFiltradas.forEach(pelicula => {
@@ -20,7 +16,6 @@ fetch('data/buscador.json')
     listaPeliculas.style.display = peliculasFiltradas.length > 0 ? 'block' : 'none';
     }
 
-    // Evento input del campo de búsqueda
     buscador.addEventListener('input', function() {
         const filtro = buscador.value.toLowerCase();
         const peliculasFiltradas = peliculas.filter(pelicula =>
